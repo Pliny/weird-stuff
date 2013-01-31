921c699fde124b187e128cf28c77f882d9967a44
Dave Desrochers <dave726@gmail.com>
2013-01-30 20:35:22 -0800

    User sees correct landing page (no fb link yet)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ WeirdStuff::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -19,6 +19,10 @@ WeirdStuff::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.assets.enabled = true
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :yui
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -43,7 +47,7 @@ WeirdStuff::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "weird-stuff.herokuapp.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
