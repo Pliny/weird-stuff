09a178b8ba43b6b3953ce5f041682255ac595461
Dave Desrochers <dave726@gmail.com>
2013-02-18 14:24:25 -0800

    Fixed firefox angles. they were missing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,10 @@ module ApplicationHelper
 
   def bg_color_of page
     @bg_color_of ||= {
-      header:   "bg-color%02d" % ((page)   % COLORS),
-      angle:    "angle%02d"    % ((page)   % COLORS),
-      footer:   "bg-color%02d" % ((page+1) % COLORS),
+      header:       "bg-color%02d" % ((page)   % COLORS),
+      top_angle:    "angle%02d"    % ((page)   % COLORS),
+      bottom_angle: "angle%02d"    % ((page+1) % COLORS),
+      footer:       "bg-color%02d" % ((page+1) % COLORS),
       font:     page == 0 ? 'initial-color'          : "color%02d"          % ((page%COLORS == 0) ? COLORS-1 : (page%COLORS)-1),
       facebook: page == 0 ? 'initial-share-facebook' : "share-facebook%02d" % ((page%COLORS == 0) ? COLORS-1 : (page%COLORS)-1),
       twitter:  page == 0 ? 'initial-share-twitter'  : "share-twitter%02d"  % ((page%COLORS == 0) ? COLORS-1 : (page%COLORS)-1)
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,56 +8,61 @@ describe ApplicationHelper do
 
   it "should choose the page's color" do
     bg_color_of(4).should == {
-      header:   'bg-color04',
-      angle:    'angle04',
-      footer:   'bg-color05',
-      font:     'color03',
-      facebook: 'share-facebook03',
-      twitter:  'share-twitter03'
+      header:       'bg-color04',
+      top_angle:    'angle04',
+      bottom_angle: 'angle05',
+      footer:       'bg-color05',
+      font:         'color03',
+      facebook:     'share-facebook03',
+      twitter:      'share-twitter03'
     }
   end
 
   it "should wrap at 14" do
     bg_color_of(13).should == {
-      header:   'bg-color13',
-      angle:    'angle13',
-      footer:   'bg-color00',
-      font:     'color12',
-      facebook: 'share-facebook12',
-      twitter:  'share-twitter12'
+      header:       'bg-color13',
+      top_angle:    'angle13',
+      bottom_angle: 'angle00',
+      footer:       'bg-color00',
+      font:         'color12',
+      facebook:     'share-facebook12',
+      twitter:      'share-twitter12'
     }
   end
 
   it "should handle the special case when page is 0" do
     bg_color_of(0).should == {
-      header:   'bg-color00',
-      angle:    'angle00',
-      footer:   'bg-color01',
-      font:     'initial-color',
-      facebook: 'initial-share-facebook',
-      twitter:  'initial-share-twitter'
+      header:       'bg-color00',
+      top_angle:    'angle00',
+      bottom_angle: 'angle01',
+      footer:       'bg-color01',
+      font:         'initial-color',
+      facebook:     'initial-share-facebook',
+      twitter:      'initial-share-twitter'
     }
   end
 
   it "should handle large page numbers" do
     bg_color_of(42).should == {
-      header:   'bg-color00',
-      angle:    'angle00',
-      footer:   'bg-color01',
-      font:     'color13',
-      facebook: 'share-facebook13',
-      twitter:  'share-twitter13'
+      header:       'bg-color00',
+      top_angle:    'angle00',
+      bottom_angle: 'angle01',
+      footer:       'bg-color01',
+      font:         'color13',
+      facebook:     'share-facebook13',
+      twitter:      'share-twitter13'
     }
   end
 
   it "should handle large page numbers 2" do
     bg_color_of(15).should == {
-      header:   'bg-color01',
-      angle:    'angle01',
-      footer:   'bg-color02',
-      font:     'color00',
-      facebook: 'share-facebook00',
-      twitter:  'share-twitter00'
+      header:       'bg-color01',
+      top_angle:    'angle01',
+      bottom_angle: 'angle02',
+      footer:       'bg-color02',
+      font:         'color00',
+      facebook:     'share-facebook00',
+      twitter:      'share-twitter00'
     }
   end
 end
